bed378d76d27620a65184ab0ab6b9e6f13c5be49
zestermo <zach.stermon@gmail.com>
2025-12-09 15:01:11 -0500

    pushing env changes

--- backend/config/database.php
+++ backend/config/database.php
@@ -45,12 +45,12 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('DATABASE_URL', env('DB_URL')),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'laravel'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
+            'url' => env('MYSQL_PUBLIC_URL', env('DB_URL')),
+            'host' => env('MYSQLHOST', '127.0.0.1'),
+            'port' => env('MYSQLPORT', '3306'),
+            'database' => env('MYSQL_DATABASE', 'laravel'),
+            'username' => env('MYSQLUSER', 'root'),
+            'password' => env('MYSQL_ROOT_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => env('DB_CHARSET', 'utf8mb4'),
             'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
